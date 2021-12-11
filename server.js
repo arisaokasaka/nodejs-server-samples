b841ffa8334e8ffe202b7cfdfb5e6191b2850be4
Arisa Okasaka <68284764+arisaokasaka@users.noreply.github.com>
2021-12-12 00:07:06 +0900

    create server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const server = http.createServer(function(_req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.write('Hello World')
+    res.end()
+})
+
+server.listen(1224)
+console.log('サーバーを起動しました')
